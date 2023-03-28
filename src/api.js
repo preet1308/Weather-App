ee0ae38c61139168cd86c226ea5e1289de157d7a
preet1308 <122307836+preet1308@users.noreply.github.com>
2023-03-28 11:39:15 +0530

    Weather Forecast for major locations for a week
    Suggestions and country code for guided search.

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd4c484b22amshfe49b30894efebap1c301fjsn17955ea846e5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2ef9c01c2e2dda9a464c5ca29f3bb6e5"; 
